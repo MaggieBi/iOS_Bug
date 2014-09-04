287ec21d11b72e9a8d1c215f85ecce841154fe82
Maggie Yunjie Bi <MaggieBi@Maggies-MacBook-Air.local>
2014-09-03 21:51:00 -0400

    Initial Commit

---
+++ Picnic_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Picnic_1
+//
+//  Created by Maggie Yunjie Bi on 9/3/14.
+//  Copyright (c) 2014 daapr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
